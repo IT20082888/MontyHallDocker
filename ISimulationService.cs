34e7b1265704260e56be580f698ab3f4206e40d1
Osanda Randunu <89739995+IT20082888@users.noreply.github.com>
2024-01-31 11:55:00 +0530

    monity hall simulation files

---
+++ ISimulationService.cs
@@ -0,0 +1,11 @@
+﻿using Back.Model;
+
+namespace Back
+{
+    public interface ISimulationService
+    {
+        List<GameResult> SimulateGames(int numberOfSimulations, bool changeDoor);
+
+    }
+
+}
